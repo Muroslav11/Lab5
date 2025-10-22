dcd55de2af441821b0a05f4da44406464b98f4c3
Muroslav11 <Muroslavhalik@gmail.com>
2025-10-22 22:02:37 +0300

    lab5.2

---
+++ Lab5.2/Lab5.2/Lab5.2.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+// Прототипи функцій
+// Функція для обчислення суми ряду S
+void S(const double x, const double eps, int& n, double& s);
+// Функція для обчислення наступного доданка A (використовуючи рекурентне співвідношення)
+void A(const double x, const int n, double& a);
+
+
+int main()
+{
+    double xp, xk, x, dx, eps, s = 0;
+    int n = 0;
+
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+
+    // Заголовок таблиці
+    cout << "--------------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(15) << "Arctanh(x)" << " |"
+        << setw(15) << "S" << " |"
+        << setw(10) << "n" << " |"
+        << endl;
+    cout << "--------------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        if (abs(x) >= 1.0) {
+            cout << "|" << setw(7) << setprecision(2) << x << " |"
+                << setw(15) << "undefined" << " |"
+                << setw(15) << "---" << " |"
+                << setw(10) << "---" << " |"
+                << endl;
+            x += dx;
+            continue;
+        }
+
+        // Обчислення суми S та кількості доданків n
+        S(x, eps, n, s);
+
+        // Виведення результатів
+        // Функція atanh(x) у C++ обчислює арктангенс гіперболічний
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(15) << setprecision(10) << atanh(x) << " |"
+            << setw(15) << setprecision(10) << s << " |"
+            << setw(10) << n << " |"
+            << endl;
+
+        x += dx;
+    }
+
+    cout << "--------------------------------------------------------" << endl;
+    return 0;
+}
+
+
+void S(const double x, const double eps, int& n, double& s)
+{
+   
+    n = 0;
+    double a = x;
+    s = a;
+
+    do { 
+        A(x, n, a);
+
+        s += a; 
+
+        n++;
+
+    } while (abs(a) >= eps); // Умова зупинки
+}
+
+void A(const double x, const int n, double& a)
+{
+    double R = (x * x) * (2.0 * n + 1.0) / (2.0 * n + 3.0);
+
+    a *= R;
+}
---
+++ Lab5.2/UnitTest5.2/UnitTest5.2.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+#include "../Lab5.2/Lab5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace UnitTest52
+{
+    TEST_CLASS(UnitTest52)
+    {
+    public:
+
+		// Тест 1: перевірка функції A() — рекурентного співвідношення
+		TEST_METHOD(Test_A_Function)
+		{
+			double a = 0.1;  // початковий доданок
+			double x = 0.5;
+			int n = 0;
+
+			A(x, n, a);  // виклик функції A()
+
+			// очікуване значення:
+			// R = (x^2) * (2n + 1) / (2n + 3)
+			// R = (0.25) * (1) / (3) = 0.0833333
+			// a*= R
+			double expected = 0.1 * (0.25 * 0.33333333);
+
+			Assert::AreEqual(expected, a, 1e-7);
+		}
+
+		TEST_METHOD(Test_S_Function)
+		{
+			double x = 0.3;
+			double eps = 1e-6;
+			int n;
+			double s;
+
+			S(x, eps, n, s);  // обчислення суми ряду
+
+			// Очікуване значення — через стандартну функцію atanh(x)
+			double expected = atanh(x);
+
+			Assert::AreEqual(expected, s, 1e-5);
+		}
+
+    };
+}
