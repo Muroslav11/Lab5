05132f98868b13dd8bd2b0aa45ead2893f15f2aa
Muroslav11 <Muroslavhalik@gmail.com>
2025-11-04 01:40:09 +0200

    Lab5.3

---
+++ Lab5.3/Lab5.3/Lab5.3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double y(const double x);
+
+// Основна функція main()
+int main() 
+{
+    double fp, fk; // початок і кінець інтервалу
+    int n;         // кількість відрізків
+
+    cout << "fp = "; cin >> fp;
+    cout << "fk = "; cin >> fk;
+    cout << "n = "; cin >> n;
+
+    double df = (fk - fp) / n;
+
+    
+    cout << "------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |" << setw(30) << "y(f/2)+y(f+1)^2+y(2*f)" << " |\n";
+    cout << "------------------------------------------" << endl;
+
+
+    for (double f = fp; f <= fk; f += df)
+    {
+        double result = y(f / 2) + pow(y(f + 1), 2) + y(2 * f);
+        cout << "|" << setw(7) << f << " |" << setw(30) << result << " |\n";
+    }
+    cout << "------------------------------------------" << endl;
+    
+    return 0;
+}
+
+
+
+// Допоміжна функція y(x)
+double y(const double x) 
+{
+    if (abs(x) >= 1)
+        return (exp(x) / (1 + exp(x) + sin(x)));
+    else 
+    {
+        double S = 1;      
+        double a = 1;      
+        for (int j = 1; j <= 8; j++)
+        {
+            a *= -x * x / ((2 * j - 1) * (2 * j));  // рекурентне співвідношення
+            S += a;
+        }
+        return S;
+    }
+}
---
+++ Lab5.3/UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+#include "../Lab5.3/Lab5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		// |x| >= 1 — перевірка формули (наприклад x = 1)
+		TEST_METHOD(Test_Y_argument_equis_one)
+		{			
+			double expected = exp(1.0) / (1 + exp(1.0) + sin(1.0));
+			double result = y(1.0);
+
+			Assert::AreEqual(expected, result);
+		}
+
+		// |x| < 1 — перевірка y(0) = 1 (ряд Тейлора)
+		TEST_METHOD(Test_Y_Zero)
+		{
+			Assert::AreEqual(1.0, y(0.0));
+		}
+	};
+}
