05802137e3ff097a605ab0b9e0fa5abcb980b2b8
Muroslav11 <Muroslavhalik@gmail.com>
2025-11-07 17:16:46 +0200

    Lab5.5

---
+++ Lab5.5/Lab5.5/Lab5.5.cpp
@@ -0,0 +1,29 @@
+﻿// Лабораторна робота №5.5
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// --- Рекурсивна функція ---
+double power(double x, int n)
+{
+    if (n == 0)
+        return 1;                        
+    else if (n < 0)
+        return 1.0 / power(x, abs(n));       
+    else
+        return x * power(x, n - 1);   
+}
+
+int main()
+{
+    double x;
+    int n;
+
+    cout << "x = "; cin >> x;
+    cout << "n = "; cin >> n;
+    cout << "x^n = " << power(x, n) << endl;
+
+    return 0;
+}
---
+++ Lab5.5/UnitTest5.5/UnitTest5.5.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.5/Lab5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest55
+{
+	TEST_CLASS(UnitTest55)
+	{
+	public:
+		
+        // Тест: n = 0 → x^0 = 1
+        TEST_METHOD(Test_Power_ZeroExponent)
+        {
+            double result = power(5.0, 0);
+            Assert::AreEqual(1.0, result);
+        }
+
+        // Тест: Додатний показник → 2^3 = 8
+        TEST_METHOD(Test_Power_PositiveExponent)
+        {
+            double result = power(2.0, 3);
+            Assert::AreEqual(8.0, result);
+        }
+
+        // Тест: Від’ємний показник → 2^-2 = 1/4
+        TEST_METHOD(Test_Power_NegativeExponent)
+        {
+            double result = power(2.0, -2);
+            Assert::AreEqual(0.25, result, 1e-10); // з точністю
+        }
+	};
+}
